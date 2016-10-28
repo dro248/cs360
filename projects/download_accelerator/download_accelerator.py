322561f4c5a7cc126b6fb2e1d5a221a0f88b864e
Santiago Verdu <santiagoverdu@protonmail.com>
2016-10-27 19:45:19 -0600

    update gitignore and started download accelerator

--- projects/download_accelerator/download_accelerator.py
+++ projects/download_accelerator/download_accelerator.py
@@ -3,44 +3,102 @@
 import threading
 import sys, logging
 import argparse
-import re
+import requests
+
+class Dl_Range(threading.Thread):
+    def __init__(self,dl_range):
+        threading.Thread.__init__(self)
+        if dl_range[0] >= dl_range[1] or dl_range[0] < 0:
+            logging.error("Invalid range given: %d-%d" % (dl_range[0], dl_range[1]))
+            self.range = None
+        else:
+            logging.debug("Range set to: %d-%d" % (dl_range[0], dl_range[1]))
+            self.range = dl_range
+
+    def run(self):
+        """ Download the range that is specified """
+        self.gen_headers()
+
+    def gen_headers(self):
+        if self.range is None:
+            logging.error("Range has not been defined")
+        else:
+            headers = {}
+            headers["Accept-Encoding"] = "identity"
+            headers["Range"] = "bytes=%d-%d" % self.range
+            logging.debug("HEADERS: %s" % str(headers))
+            return headers
 
 class Download_Manager():
     """ handles the threading and organizing for a download """
     def __init__(self,num_threads,url):
         self.threads = []
-    def get_content_length():
+        self.content_length = 0
+        self.url = url
+        self.num_threads = num_threads
+        self.manage()
+
+    def get_content_length(self):
         """ Makes a HEAD request to the url's web server for the Content-Length """
-    def manage():
+        head = requests.head(self.url)
+        self.content_length = int(head.headers["Content-Length"])
+        logging.debug("Content-Length: %s" % self.content_length)
+
+    def manage(self):
         """ The main logic for the downloader """
-    def make_threads():
+        self.get_content_length()
+        if self.content_length is 0:
+            logging.error("Content-Length header not set. Non-Static site. Download Failed.")
+            return
+        else:
+            self.make_threads()
+            self.download()
+            
+    def gen_ranges(self):
+        """ Generates the ranges using python2 stuff """
+        range_length = self.content_length // self.num_threads
+        if range_length > 0:
+            ranges = []
+            prev_end = -1
+            for x in xrange(0,self.num_threads):
+                beg = prev_end + 1
+                prev_end += range_length
+                end = prev_end
+                if x is self.num_threads-1:
+                    if end < self.content_length:
+                        logging.debug("Final Range before change: %d" % end)
+                        end = self.content_length
+                        logging.debug("Final Range changed to %d" % end)
+                ranges.append((beg,end))
+            return ranges
+        else:
+            logging.critical("More threads specified than bytes in download.\nExiting.")
+            sys.exit(1)
+
+    def make_threads(self):
         """ Makes the threads for the download """
-    def download():
-        """ Creates the threads and downloads the file. Then it """
-    def assemble():
+        ranges = self.gen_ranges()
+        for dl_range in ranges:
+            self.threads.append(Dl_Range(dl_range))
+
+    def download(self):
+        """ starts all of the threads """
+        for thread in self.threads:
+            thread.start()
+
+    def assemble(self):
         """ Puts the downloaded content back together again """
 
 def parse_options():
     parser = argparse.ArgumentParser(prog="Download Accelerator", description="Threaded Static File Downloader", add_help=True)
     parser.add_argument("-n", "--number", type=int, action="store", help="Specify the number of threads to create",default=10)
+    parser.add_argument("url", help="The url of the page that you want to download")
     parser.add_argument("-d", "--debug", action="store_true", help="Turn on logging")
     return parser.parse_args() 
 
-def isUrl(string):
-    """ Check to see if this is the url param
-    It's not important to check for more than just http 
-    because this is the only param that we will need to check """
-    return True if string.startswith("http") else False
-
-def get_url():
-    logging.debug("Getting the URL")
-    logging.debug(sys.argv)
-
 if __name__ == "__main__":
     args = parse_options()
-    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.ERROR)
     num_threads = args.number if args.number > 0 else 1
-    logging.debug("number of threads %d" % num_threads)
-    get_url()
-    dlmngr = Download_Manager(args.url, num_threads=num_threads)
+    dlmngr = Download_Manager(num_threads=num_threads, url=args.url)
 
